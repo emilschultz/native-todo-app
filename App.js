8ea650cfe3961d7d1affdbac52efe265f4a6fb95
Emil Schultz <emilrschultz@gmail.com>
2021-03-23 11:45:33 +0100

    puah test

--- App.js
+++ App.js
@@ -2,20 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App () =>
+ {
+  return(
+    <View>
+      <Text>Heyhey, dette er min native app</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
